868b6d167cb0e7a3273a610beab8c4a726899f68
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-10-23 12:32:02 -0400

    local db read write working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,11 +17,14 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
+// app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
